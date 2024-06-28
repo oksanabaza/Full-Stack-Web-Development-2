631216d1a4f37b7031e514fa1f5d1e788ede9049
oksanabaza <obazylie@redhat.com>
2024-06-28 18:24:37 +0100

    Exercise - Show favourite icon on Movie Details page..

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -5,7 +5,9 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
-import { MovieDetailsProps } from "../../types/interfaces"; 
+import { MovieDetailsProps } from "../../types/interfaces";
+import FavoriteIcon from "@mui/icons-material/Favorite"; 
+import Avatar from "@mui/material/Avatar";
 
 const styles = {
     root: {  
@@ -14,11 +16,15 @@ const styles = {
     alignItems: "center",
     flexWrap: "wrap",
     padding: 1.5,
+    avatar: {
+      backgroundColor: "rgb(255, 0, 0)",
+    },
   },
 };
 
 const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
-  
+  const favourites = JSON.parse(localStorage.getItem("favourites") || '[]');
+  const isFavourite = favourites.find((favourite: { id: number; }) => favourite.id === movie.id);
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
@@ -33,6 +39,13 @@ const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
         <br />
         <span>{`${movie.tagline}`} </span>
       </Typography>
+      {
+          isFavourite ? (
+            <Avatar sx={styles.root.avatar}>
+              <FavoriteIcon />
+            </Avatar>
+          ) : null
+        }
       <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
