c803a8fc31bb0ecda8fae5ff05fb29baeab9ce72
oksanabaza <obazylie@redhat.com>
2024-06-30 09:56:12 +0100

    Added Upcoming movies page.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader'
+import SiteHeader from './components/siteHeader';
+import UpcomingMoviePage from "./pages/upcomingMoviesPage";
 
 const App = () => {
   return (
@@ -13,10 +14,12 @@ const App = () => {
       <SiteHeader />
       <Routes>
       <Route path="/reviews/:id" element={<MovieReviewPage/>} />
+      <Route path="movies/upcoming" element={<UpcomingMoviePage />} />
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
+        
       </Routes>
     </BrowserRouter>
   );
