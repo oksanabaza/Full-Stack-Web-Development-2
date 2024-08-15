bf20f8b305ad9d212f54a38432d494dbf0c7bab7
oksanabaza <obazylie@redhat.com>
2024-08-15 11:11:45 +0100

    Update theme and create theme toggle

--- src/components/siteHeader/index.tsx
+++ src/components/siteHeader/index.tsx
@@ -9,23 +9,28 @@ import MenuIcon from "@mui/icons-material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import { useNavigate } from "react-router-dom";
-import { useTheme } from "@mui/material/styles";
+import { useTheme, Theme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import { Container } from '@mui/material';
+import { Container } from "@mui/material";
+import ThemeToggle from '../../themeToggle';
 
 const styles = {
-    title: {
-      flexGrow: 1,
-    },
-  };
+  title: {
+    flexGrow: 1,
+  },
+};
+
+interface HeaderProps {
+  theme: Theme;
+  toggleTheme: () => void;
+}
 
 const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
-const SiteHeader: React.FC = () => {
+const SiteHeader: React.FC<HeaderProps> = ({ theme, toggleTheme }) => {
   const navigate = useNavigate();
-  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement|null>(null);
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
   const open = Boolean(anchorEl);
-  const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
 
   const menuOptions = [
@@ -48,68 +53,69 @@ const SiteHeader: React.FC = () => {
   return (
     <>
       <AppBar position="fixed" elevation={0} color="primary">
-      <Container maxWidth="xl">
-        <Toolbar>
-          <Typography variant="h4" sx={styles.title}>
-            TMDB Client
-          </Typography>
-          <Typography variant="h6" sx={styles.title}>
-            All you ever wanted to know about Movies!
-          </Typography>
-          {isMobile ? (
-            <>
-              <IconButton
-                aria-label="menu"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-                size="large"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={open}
-                onClose={() => setAnchorEl(null)}
-              >
+        <Container maxWidth="xl">
+          <Toolbar>
+            <Typography variant="h4" sx={styles.title}>
+              TMDB Client
+            </Typography>
+            <Typography variant="h6" sx={styles.title}>
+              All you ever wanted to know about Movies!
+            </Typography>
+            {isMobile ? (
+              <>
+                <IconButton
+                  aria-label="menu"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                  size="large"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={open}
+                  onClose={() => setAnchorEl(null)}
+                >
+                  {menuOptions.map((opt) => (
+                    <MenuItem
+                      key={opt.label}
+                      onClick={() => handleMenuSelect(opt.path)}
+                    >
+                      {opt.label}
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>
+            ) : (
+              <>
                 {menuOptions.map((opt) => (
-                  <MenuItem
+                  <Button
                     key={opt.label}
+                    color="inherit"
                     onClick={() => handleMenuSelect(opt.path)}
                   >
                     {opt.label}
-                  </MenuItem>
+                  </Button>
                 ))}
-              </Menu>
-            </>
-          ) : (
-            <>
-              {menuOptions.map((opt) => (
-                <Button
-                  key={opt.label}
-                  color="inherit"
-                  onClick={() => handleMenuSelect(opt.path)}
-                >
-                  {opt.label}
-                </Button>
-              ))}
-            </>
-          )}
-        </Toolbar>
+              </>
+            )}
+            <ThemeToggle theme={theme.palette.mode} toggleTheme={toggleTheme} />
+          </Toolbar>
         </Container>
       </AppBar>
-      
+
       <Offset />
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React,{useState} from "react";
-import ReactDOM from 'react-dom/client'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
@@ -7,70 +7,75 @@ import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader';
 import UpcomingMoviePage from "./pages/upcomingMoviesPage";
-import TvShowsPage from '../src/pages/tvShowsPage'
+import TvShowsPage from './pages/tvShowsPage';
 import { QueryClientProvider, QueryClient } from "react-query";
-import { ReactQueryDevtools } from 'react-query/devtools';
+import { ReactQueryDevtools } from "react-query/devtools";
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage';
 import PopularMoviesPage from "./pages/PopularMoviesPage";
-import TVSeriesPage from "./pages/TVSeriesPage"
-import ActorsPage from './pages/ActorsPage'
+import TVSeriesPage from "./pages/TVSeriesPage";
+import ActorsPage from './pages/ActorsPage';
 import LoginPage from './pages/loginPage';
 import ProtectedRoute from './components/protectedRoute';
 import AuthProvider from "./contexts/authContext";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline, Container } from "@mui/material";
+import { lightTheme, darkTheme } from './theme';
 import "./styles.css"; 
-import { Container } from '@mui/material';
-
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 360000,
-      refetchInterval: 360000, 
-      refetchOnWindowFocus: false
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false,
     },
   },
 });
 
-const App = () => {
-  return ( 
-  <ThemeProvider theme={theme}>
-    <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-    <AuthProvider>
-      
-      <SiteHeader />
-      <MoviesContextProvider>
-      <Container maxWidth="xl">
-      <Routes>
-      <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
-      <Route path="/reviews/:id" element={<MovieReviewPage/>} />
-      <Route path="movies/upcoming" element={<UpcomingMoviePage />} />
-        {/* <Route path="/movies/favourites" element={<FavouriteMoviesPage />} /> */}
-        <Route path="/movies/favourites" element={
-              <ProtectedRoute>
-                <FavouriteMoviesPage />
-              </ProtectedRoute>
-            } />
-        <Route path="/movies/popular" element={<PopularMoviesPage />} />
-        <Route path="/movies/tvseries" element={<TVSeriesPage />} />
-        <Route path="/movies/people" element={<ActorsPage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-        <Route path="/actors/:id" element={<ActorsPage />} />
-        <Route path="/tvshows/:id" element={<TvShowsPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-        
-      </Routes>
-     </Container>
-      </MoviesContextProvider>
-      </AuthProvider>
-    </BrowserRouter>
-    <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+const App: React.FC = () => {
+  const [themeMode, setThemeMode] = useState<'light' | 'dark'>('light');
+
+  const toggleTheme = () => {
+    setThemeMode((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  const currentTheme = themeMode === 'light' ? lightTheme : darkTheme;
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <CssBaseline />
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AuthProvider>
+            <SiteHeader theme={currentTheme} toggleTheme={toggleTheme} />
+            <MoviesContextProvider>
+              <Container maxWidth="xl">
+                <Routes>
+                  <Route path="/reviews/form" element={<AddMovieReviewPage />} />
+                  <Route path="/reviews/:id" element={<MovieReviewPage />} />
+                  <Route path="movies/upcoming" element={<UpcomingMoviePage />} />
+                  <Route path="/movies/favourites" element={
+                    <ProtectedRoute>
+                      <FavouriteMoviesPage />
+                    </ProtectedRoute>
+                  } />
+                  <Route path="/movies/popular" element={<PopularMoviesPage />} />
+                  <Route path="/movies/tvseries" element={<TVSeriesPage />} />
+                  <Route path="/movies/people" element={<ActorsPage />} />
+                  <Route path="/movies/:id" element={<MoviePage />} />
+                  <Route path="/actors/:id" element={<ActorsPage />} />
+                  <Route path="/tvshows/:id" element={<TvShowsPage />} />
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+              </Container>
+            </MoviesContextProvider>
+          </AuthProvider>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 };
@@ -78,5 +83,5 @@ const App = () => {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
