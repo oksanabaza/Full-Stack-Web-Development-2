3462ed949c34c85cb1acea69a613c2e02df1e70d
oksanabaza <obazylie@redhat.com>
2024-08-15 11:19:57 +0100

    Update styles according themes

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,33 +1,3 @@
-// import { createTheme } from '@mui/material/styles';
-
-// const theme = createTheme({
-//   palette: {
-//     background: {
-//       default: '#1a1a1a', 
-//       paper: '#333333', 
-//     },
-//     text: {
-//       primary: '#ffffff',
-//     },
-//     primary: {
-//       main: '#1976d2', 
-//       dark: '#115293', 
-//     },
-//   },
-//   components: {
-
-//     MuiCard: {
-//       styleOverrides: {
-//         root: {
-//           backgroundColor: '#333333',
-//         },
-//       },
-//     },
-//   },
-// });
-
-// export default theme;
-// theme.js
 import { createTheme } from '@mui/material/styles';
 
 export const lightTheme = createTheme({
